b53d122ad5dbaed3e8bf6ea90c6f8d5f1a209013
Luis Manzur <luismanzur91@outlook.com>
2022-03-19 19:05:50 -0400

    all set

--- worldwidebooking/settings.py
+++ worldwidebooking/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x+oa7ok9tsks#n2y$mlxr3cgk%%w@pbh(-te64j9_mj+58gsf3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -152,6 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
